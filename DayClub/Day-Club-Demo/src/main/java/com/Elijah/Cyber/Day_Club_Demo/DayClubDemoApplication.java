2e6a852f0ea0d597d8ac68817935b886b316ac72
Elijah-cyber7 <63973997+Elijah-cyber7@users.noreply.github.com>
2024-05-26 15:14:49 -0500

    Added SpringBoot boiler plate

---
+++ DayClub/Day-Club-Demo/src/main/java/com/Elijah/Cyber/Day_Club_Demo/DayClubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Elijah.Cyber.Day_Club_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayClubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayClubDemoApplication.class, args);
+	}
+
+}
---
+++ DayClub/Day-Club-Demo/src/test/java/com/Elijah/Cyber/Day_Club_Demo/DayClubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Elijah.Cyber.Day_Club_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayClubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
